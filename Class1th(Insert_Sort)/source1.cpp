12b16afcb6a05eeaf9317ac45b9f8b467817d730
hallow0312 <leejaehyun0312@naver.com>
2024-04-10 21:46:16 +0900

    2024.04.10.계수정렬

---
+++ Class1th(Insert_Sort)/source1.cpp
@@ -0,0 +1,103 @@
+﻿#include<iostream>
+using namespace std;
+
+#define SIZE 5
+int main()
+{
+#pragma region 거품 정렬(Bubble Sort)
+
+	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘 
+
+	// 거품 정렬의 시간복잡도 : O(n^2)
+
+	/*
+	int arr[SIZE] = {12,21,45,11,0 };
+
+
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < (SIZE-i)-1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+	cout << "Bubble sort : [ ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "]";
+	*/
+#pragma endregion
+
+#pragma region 선택정렬(Selection Sort)
+	// 주어진 리스트 중에 최솟값을 찾아서 맨 앞에 위치 한 값과 교체하는 방식으로 
+	// 정렬하는 알고리즘 
+	// 시간복잡도 (O^2)
+
+	/*int arr[SIZE] = { 21,11,4,1,17 };
+
+	for (int i = 0; i < SIZE-1; i++)
+	{
+		int min_index = i;
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (arr[min_index] > arr[j])
+			{
+				min_index = j;
+			}
+		}
+		swap(arr[i], arr[min_index]);
+
+		cout << i+1<< " 번째 회전 : [";
+		for (int k = 0; k < SIZE; k++)
+		{
+			cout << arr[k] << " ";
+		}
+		cout <<"]"<< endl;
+	}
+
+	cout << endl<<"Selection Sort : [ ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "]"<<endl;*/
+#pragma endregion
+
+#pragma region 삽입정렬(Insert Sort)
+	// data 를 하나 씩 확인하면서 이미 정렬된 부분과 비교하여 
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘 
+
+	// 시간복잡도 O(n^2)
+	/*int list[SIZE] = { 9,6,8,1,3};
+
+	int key;
+	int j;
+	for ( int i = 1; i < SIZE; i++)
+	{
+		key = list[i];
+		for ( j = i - 1; j >= 0&&list[j]>key; j--)
+		{
+			list[j + 1] = list[j];
+		}
+		list[j + 1] = key;
+		cout << i << "번째 회전 : ";
+		for (int k = 0; k < SIZE; k++)
+		{
+			cout << list[k] << " ";
+		}
+		cout << endl;
+	}
+	cout << "Insert Sort : ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout <<   list[i] << " ";
+	}*/
+#pragma endregion
+
+	return 0;
+}
---
+++ Class2th(count_sort)/souce2.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+
+#define SIZE 5
+int main()
+{
+#pragma region 계수 정렬 
+	// 데이터의 값을 직접 비교하지 않고 
+	// 단순하게 각 숫자가 몇개 있는지 개수를 세어 저장한 다음 ,
+	// 정렬하는 알고리즘 
+
+	/*
+	int arr[] = { 1,2,3,4,1,1,5};
+	int arr_count[SIZE] = { 0, };
+
+	int arr_size = sizeof(arr) / sizeof(int);
+	for (int i = 0; i < arr_size; i++)
+	{
+		arr_count[arr[i] - 1] = ++arr_count[arr[i] - 1];
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr_count[i] << " ";
+	}
+	*/
+
+
+
+
+#pragma endregion
+#pragma region 문자열 뒤집기 
+
+	/*
+	string s;
+	cin >> s;
+	cout << s << " : ";
+	for (int i = 0; i < s.length()/2; i++)
+	{
+		swap(s[i], s[s.length() - 1 - i]);
+	}
+	cout << s;
+	*/
+#pragma endregion
+
+
+	return 0;
+}
