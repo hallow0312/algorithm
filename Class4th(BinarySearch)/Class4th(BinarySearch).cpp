a61a2f05b7341bde36c153c9eb999ab807b92c8e
hallow0312 <leejaehyun0312@naver.com>
2024-04-15 21:44:55 +0900

    2024.04.15.동적계획법

---
+++ Class4th(BinarySearch)/Class4th(BinarySearch).cpp
@@ -0,0 +1,109 @@
+﻿#include<iostream>
+using namespace std;
+#define SIZE 7
+
+void BinarySearch(int arr[], int left, int right, int key)
+{
+	if (left > right)
+	{
+		cout << "Can't Find " << key << endl;
+		return;
+	}
+
+	int count = (left + right) / 2;
+
+	if (arr[count] == key)
+	{
+		cout << "Find " << arr[count] << endl;
+		return;
+	}
+	else if (arr[count] < key)
+	{
+		left = count + 1;
+	}
+	else if (arr[count] > key)
+	{
+		right = count - 1;
+	}
+	return BinarySearch(arr, left, right, key);
+}
+int main()
+{
+#pragma region 이진탐색 
+
+	//오름 차순으로 정렬 된 리스트에서 특정한 값의 위치를 찾는 알고리즘.
+
+	//for 문으로 원하는 숫자를 찾게 되면 시간복잡도 O(n)
+
+	// 이진 탐색으로 찾게 되면 시간복잡도 O(lg n)
+	/*int buffer[SIZE] = { 5,6,11,13,27,55,88 };
+	int key;
+	cin >> key;
+	int left = 0;
+	int right = SIZE - 1;
+
+	BinarySearch(buffer, left, right, key);*/
+	/*
+	while (1)
+	{
+		//1. 배열의 가운데 요소의 인덱스를 key 로 설정
+		int count = (left + right) / 2;
+		// left 값이 right 값 보다 크면 탐색 중단
+		if (left > right)
+		{
+			cout << "Can't Find " << key;
+			break;
+		}
+		//buffer[count] 값이 key  와 같으면 검색 종료
+		if (buffer[count] == key)
+		{
+			cout << "Find "<< buffer[count] << endl;
+			break;
+		}
+		else if (buffer[count] > key)	 // 찾고자하는 key 값이 buffer[count] 값보다 작다면 left~count-1 사이의 값을 검색하게  해준다
+		{
+			right = count - 1;
+		}
+		else if (buffer[count] < key)	// 찾고자하는 key 값이 buffer[count] 보다 크다면 count+1~right 사이의 값을 검색하게 해준다 해준다 .
+		{
+			left = count + 1;
+		}
+	}
+	*/
+
+
+#pragma endregion
+
+#pragma region 단일 책임 원칙 
+
+	// 하나의 객체는 반드시 하나의 책임만 가지고 있어야 함 .
+	// 하나의 클래스가 여러 책임을 가지고 있다면 , 클래스를 분리하여야 함 .
+
+#pragma endregion
+
+#pragma region 개방 폐쇄 원칙 
+	// 클래스는 확장에 대해서는 열려있어야하지만 수정에 대해서는 닫혀 있어야함 .
+#pragma endregion
+
+#pragma region 리스코프 치환 원칙
+	// 하위 클래스는 상위 클래스에서 가능한 행위를 수행할 수 있어야하며, 
+	// 하위클래스는 상위클래스의 행위를 변경하지 않고 확장할 수 있어함 .
+#pragma endregion
+
+#pragma region 인터페이스 분리 원칙 
+	// 클라이언트가 자신이 사용하지 않는 함수에 의존하지 않아야 하며,
+	// 인터페이스를 구체적이고 작은 단위로 분리하여 사용하는 원칙 .
+#pragma endregion
+
+#pragma region 의존 역전 원칙
+	// 상위 수준이 하위 수준에 의존하는 전통적인 관계를 
+	// 반전시킴으로써 상위 수준 모듈이 하위 수준 모듈의 
+	// 구현에 의존하지 않도록 하는 원칙 
+
+#pragma endregion
+
+
+
+
+	return 0;
+}
---
+++ Class5th(Dynamic_Programming)/Class5th(Dynamic_Programming).cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+using namespace std;
+
+#define VALUE 100000
+int Fibonacci(int* list, int x)
+{
+	if (x == 1)
+	{
+		list[x] = x;
+		return list[x];
+	}
+	else if (x < 1)
+	{
+		return 0;
+	}
+
+	if (list[x] != 0)
+	{
+		return list[x];
+	}
+
+	return list[x] = Fibonacci(list, x - 1) + Fibonacci(list, x - 2);
+}
+
+
+
+int main()
+{
+
+#pragma region 동적 계획법
+	//특정 범위 까지의 값을 구하기 위해서 
+	// 그것과 다른범위 까지의 값을 이용하여 효율적으로 값을 
+	// 구하는 알고리즘.
+
+	//메모이제이션
+	//프로그램이 동일한 계산을 반복해야 할 때, 이전에 계산한 값을 
+	// 메모리에 저장함으로써 동일한 계산을 반복 수행하는
+	// 작업을 제거하여 프로그램의 실행 속도를 향상시키는 기법 .
+#pragma endregion
+	int list[VALUE + 1] = { 0, };
+	cout << Fibonacci(list, 46) << endl;
+
+	return 0;
+}
