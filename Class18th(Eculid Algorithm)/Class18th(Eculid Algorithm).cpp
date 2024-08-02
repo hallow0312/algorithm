14b7283eb4bcbefdf18f86914bccb3de350acdff
hallow0312 <leejaehyun0312@naver.com>
2024-08-02 21:39:38 +0900

    2024.08.02.유클리드호제법

---
+++ Class18th(Eculid Algorithm)/Class18th(Eculid Algorithm).cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+int Euclid(int num1, int num2)
+{
+	int result = 0;
+	int a = max(num1, num2);
+	int b = min(num1, num2);
+	while (true)
+	{
+		if (a % b == 0)
+		{
+			result = b;
+			break;
+		}
+		int temp = a % b;
+		a = b;
+		b = temp;
+
+	}
+	return result;
+}
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << Euclid(a, b) << '\n';
+	return 0;
+
+}
